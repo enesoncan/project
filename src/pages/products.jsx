3a4575e9654baadd44af72ed1105114d54352062
enesoncan <enesoncn@gmail.com>
2019-12-08 23:05:59 +0300

    Added filter component

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import { productsData } from '../../mock/products';
 
 import ProductHeader from '../components/product-header';
+import ProductFilter from '../components/product-filter';
 import ProductCard from '../components/product-card';
 
 import SortContextProvider from '../context/sort-context';
+import FilterContextProvider from '../context/filter-context';
+
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  padding: 0 90px;
+`;
+
+const LeftContent = styled.div`
+  width: 20%;
+  position: relative;
+  display: inline-flex;
+`;
+
+const RightContent = styled.div`
+  width: 80%;
+  position: relative;
+  display: block;
+`;
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -18,7 +39,16 @@ const Products = () => {
     <>
       <SortContextProvider>
         <ProductHeader />
-        <ProductCard products={products} />
+        <Wrapper>
+          <FilterContextProvider>
+            <LeftContent>
+              <ProductFilter />
+            </LeftContent>
+            <RightContent>
+              <ProductCard products={products} />
+            </RightContent>
+          </FilterContextProvider>
+        </Wrapper>
       </SortContextProvider>
     </>
   );
